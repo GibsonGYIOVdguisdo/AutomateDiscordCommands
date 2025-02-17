1fb84aac33d1940bf2a7cf55acbe5aa5e3680c88
Gibson <Gibson570@hotmail.com>
2025-02-17 10:03:06 +0000

    Added logic for finding command

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import discord
 import dotenv
 import os
+from time import sleep
+import asyncio
 
 dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
-CHANNEL = os.getenv("CHANNEL")
+CHANNEL = int(os.getenv("CHANNEL"))
+COMMAND_NAME = os.getenv("COMMAND_NAME")
 
 class Client(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
+        channel = client.get_channel(CHANNEL)
+        for cmd in await channel.application_commands():
+            if cmd.name == COMMAND_NAME:
+                self.command = cmd
     
 client = Client()
 client.run(TOKEN)
